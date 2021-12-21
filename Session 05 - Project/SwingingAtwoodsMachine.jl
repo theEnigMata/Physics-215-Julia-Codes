65bfbac942c893bc9c5025333c96c73f54994aef
Christopher Sevilla <christophergerardsevilla@ymail.com>
2021-12-21 16:48:13 +0800

    Finalize Code

--- Session 05 - Project/SwingingAtwoodsMachine.jl
+++ Session 05 - Project/SwingingAtwoodsMachine.jl
@@ -1,55 +1,65 @@
 using Plots
 const G = 9.80665
 
-function get_dstate(s, mu)
-    ds = copy(s)
+function get_dstate(s, μ)
+    ṡ = copy(s)
 
     r = s[1]
-    theta = s[2]
+    θ = s[2]
     vr = s[3]
-    vtheta = s[4]
+    vθ = s[4]
 
-    ds[1] = vr
-    ds[2] = vtheta
-    ds[3] = (r*vtheta^2 + G*(cos(theta) - mu)) / (1.00 + mu)
-    ds[4] = - (2.00*vr*vtheta + G*sin(theta)) / r
-    return ds
+    ṡ[1] = vr
+    ṡ[2] = vθ
+    ṡ[3] = (r*vθ^2.0 + G*(cos(θ) - μ)) / (1.00 + μ)
+    ṡ[4] = - (2.00*vr*vθ + G*sin(θ)) / r
+    return ṡ
 end
 
-function RK4!(motion, init_state, dt, mu)
-    motion[:,1] = init_state
-    for i in 2:size(motion)[2]
-        ds1 = dt * get_dstate(init_state, mu)
-        ds2 = dt * get_dstate(init_state + 0.5*ds1, mu)
-        ds3 = dt * get_dstate(init_state + 0.5*ds2, mu)
-        ds4 = dt * get_dstate(init_state + ds3, mu)
-        init_state += (ds1 + 2*ds2 + 2*ds3 + ds4) / 6
+function RK4!(motion, init_state, dt, μ)
+    for i in 1:size(motion)[2]
         motion[:,i] = init_state
-        if init_state[1] <= 0.00
+
+        ds1 = dt * get_dstate(init_state, μ)
+        ds2 = dt * get_dstate(init_state + 0.5*ds1, μ)
+        ds3 = dt * get_dstate(init_state + 0.5*ds2, μ)
+        ds4 = dt * get_dstate(init_state + ds3, μ)
+        init_state += (ds1 + 2*ds2 + 2*ds3 + ds4) / 6
+
+        if init_state[1] <= 0.00 || init_state[1] >= 7.00
             break
         end
     end
 end
 
-function plot_them(motion, time)
-    x = zeros(Float64, size(time))
-    y = zeros(Float64, size(time))
-    x .= motion[1,:].*sin.(motion[2,:])
-    y .= - motion[1,:].*cos.(motion[2,:])
+function plotter(motion, time, μ)
+    r = motion[1,:]
+    θ = motion[2,:]
+    x = r.*sin.(θ)
+    y = - r.*cos.(θ)
 
-    plt1 = plot(time, motion[1,:]
-        ,label="r-position"
-        ,linecolor=:green
-    )
+    # plt1 = plot(time, motion[1,:]
+    #     ,label="r-position"
+    #     ,linecolor=:green
+    # )
     
-    plot!(plt1, time, motion[2,:]
-        ,label="r-velocity"
-        ,linecolor=:maroon
-    )
+    # plot!(plt1, time, motion[3,:]
+    #     ,label="r-velocity"
+    #     ,linecolor=:maroon
+    # )
 
+    # plt2 = plot(time, motion[2,:]
+    #     ,label="theta-position"
+    #     ,linecolor=:green
+    # )
+    
+    # plot!(plt2, time, motion[4,:]
+    #     ,label="theta-velocity"
+    #     ,linecolor=:maroon
+    # )
 
     plt3 = plot(x, y
-                ,label="m-trajectory"
+                ,label="μ = $(μ)"
                 ,linecolor=:green
     )
     plot!(plt3
@@ -59,17 +69,27 @@ function plot_them(motion, time)
         ,aspect_ratio=:equal
     )
     
+    # display(plt1);
+    # display(plt2);
     display(plt3);
 end
 
-function SwingingAtwoodsMachine(mu::Float64=1.1185, t_fin::Float64=12.500)
-    dt = 10.0^(-2.00)
-    time = 0:dt:t_fin
+function SwingingAtwoodsMachine(μ::Float64=1.1185, t_fin::Float64=12.500)
+    dt = 10.0^(-3.0)
+    time = 0.0:dt:t_fin
     sys_motion = zeros(4, length(time))
 
-    init_state = [1.00, 3.0*pi/4.00, 0.00, 0.00]
-    RK4!(sys_motion, init_state, dt, mu)
+    init_state = [1.00, pi/2.00, 0.00, 0.00]
+    RK4!(sys_motion, init_state, dt, μ)
 
-    plot_them(sys_motion, time)
+    plotter(sys_motion, time, μ)
 end
 
+function benchmark_func(μ::Float64=1.1185, t_fin::Float64=12.500)
+    dt = 10.0^(-3.0)
+    time = 0.0:dt:t_fin
+    sys_motion = zeros(4, length(time))
+
+    init_state = [1.00, pi/2.00, 0.00, 0.00]
+    RK4!(sys_motion, init_state, dt, μ)
+end
---
+++ Session 05 - Project/SwingingAtwoodsMachine.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""@author: User"""
+
+from numpy import arange, zeros, array, pi, sin, cos
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+import matplotlib.pyplot as plt
+
+def get_dstate(s, mu):
+    ds = zeros(len(s))
+    
+    r = s[0]
+    theta = s[1]
+    vr = s[2]
+    vtheta = s[3]
+    
+    ds[0] = vr
+    ds[1] = vtheta
+    ds[2] = (r*vtheta**2 + G*(cos(theta) - mu)) / (1.00 + mu)
+    ds[3] = - (2.00*vr*vtheta + G*sin(theta)) / r
+    
+    return ds
+
+
+def RK4(motion, init_state, dt, mu):
+    for i in range(motion.shape[0]):
+        motion[i] = init_state
+        
+        ds1 = dt * get_dstate(init_state, mu)
+        ds2 = dt * get_dstate(init_state + 0.5*ds1, mu)
+        ds3 = dt * get_dstate(init_state + 0.5*ds2, mu)
+        ds4 = dt * get_dstate(init_state + ds3, mu)
+        init_state += (ds1 + 2*ds2 + 2*ds3 + ds4) / 6
+
+        if init_state[0] <= 0.00 or init_state[0] >= 7.00:
+            break
+    
+    return motion
+
+def plotter(motion, time, mu):
+    r = motion[:,0]
+    theta = motion[:,1]
+    
+    x = r*sin(theta)
+    y = -r*cos(theta)
+    
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    ax.set_xlabel(r"$x$-position")
+    ax.set_ylabel(r"$y$-position")
+    ax.plot(x, y)
+    plt.tight_layout()
+    plt.show()
+
+
+def SwingingAtwoodsMachine(mu = 1.1185, t_fin = 12.50):
+    dt = 10.0**(-2.0)
+    time = arange(0.0, t_fin, dt)
+    sys_motion = zeros((len(time), 4))
+    
+    init_state = array([1.00, pi/2.00, 0.00, 0.00])
+    
+    sys_motion = RK4(sys_motion, init_state, dt, mu)
+    
+    # plotter(sys_motion, time, mu)
+
+G = 9.80665
+def main():
+    plt.close("all")
+    # SwingingAtwoodsMachine()
+    import timeit
+    print(timeit.Timer(SwingingAtwoodsMachine).timeit(number=766))
+    
+if __name__ == '__main__':
+    main()
